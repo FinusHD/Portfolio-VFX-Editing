de182736cf26e254b2005e9419c9e2e286173916
FinusHD <linus.piur@gmail.com>
2026-05-03 17:25:54 +0200

    Portfolio VFX Editing

---
+++ JS/render_data.js
@@ -0,0 +1,7 @@
+const renderData = {
+    "ST_PP_Final.png": "This render was created to practice the implementation of real Elements in a 3D render. The person was placed in the 3D environment with a Image-editing-software.",
+    "End of Planet [GAIA].png": "This render was created as a Desktop Background. All textures are procedural and newly created. The Galaxy Background is not part of the 3D Scene and was edited after the Render.",
+    "Quak. CC.png": "This render was created as a Desktop Background. This work was created with a specific colorpalette in mind.",
+    "Saturn Cell Shader Wallpaper CC.png": "This render was created as a Desktop Background. This render is aiming for a specific cell shaded cartoon style.",
+    "Cell Shade Forest Wallpaper CC.png": "This Render was created as a Desktop Background. I tried to use cell-shaded textures on every model, and it was the first time I had used a hair-particlesystem.\n"
+};
